8ecfb5410d959e40b517fff7d38ef42b762921c0
monggu03 <leoisthestart@gmail.com>
2026-04-01 21:52:38 +0900

    11650

--- baekjoon/Silver/jwapo_11650.java
+++ baekjoon/Silver/jwapo_11650.java
@@ -5,26 +5,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 
-public class Agesort_10814 {
-    public static void main(String[] args) throws Exception {
+public class jwapo_11650 {
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        String[][] list = new String[count][2];
-
+        int[][] list = new int[count][2];
         for(int i=0; i<count; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            list[i][0] = st.nextToken();
-            list[i][1] = st.nextToken();
+            list[i][0] = Integer.parseInt(st.nextToken());
+            list[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(list, (a, b) ->
-                Integer.parseInt(a[0]) - Integer.parseInt(b[0])
-        );
-
         StringBuilder sb = new StringBuilder();
+
+        Arrays.sort(list, (a,b) ->{
+            int result = a[0]-b[0];
+            if(result!=0){
+                return result;
+            }
+            return a[1]-b[1];
+
+        });
+
         for(int i=0; i<count; i++){
             sb.append(list[i][0]).append(" ").append(list[i][1]).append("\n");
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
